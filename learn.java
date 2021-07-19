76552c7aa2e8c21661169403c2142ab871664846
Bibek <karmakarbibek1998@gmail.com>
2021-07-19 21:08:19 +0530

    new change in dev

--- learn.java
+++ learn.java
@@ -4,5 +4,7 @@ public class learn{
 		System.out.println("changing the notepad");
 		System.out.println("world bank corrupted");
 		abcdefghijklmnopqrstvwxyz
+		why are u watching this.. go and learn something which will help u to make ur future.. if u waste time to watch my profile then nothing to say,. bcz i wouldn't marry u... so get lost... bye byee
+		
 	}
 }
